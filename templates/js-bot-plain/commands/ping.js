d3c04fb276251a745e4b4486cc7d4be48e4e8d8c
Ayush Chowdhury <ayush.chowdhury2004@gmail.com>
2020-12-01 10:21:14 +0530

    update command

--- templates/js-bot-plain/commands/ping.js
+++ templates/js-bot-plain/commands/ping.js
@@ -1,7 +1,22 @@
+import { Message, Client } from 'discord.js';
 module.exports = {
     name: 'ping',
-    description: 'Ping!',
+    description: 'Pings to the server to check latency!',
+    /**
+     * @description - Pings to the server to check latency.
+     * @param {Client} client - The discord.js client
+     * @param {Message} message - The message object
+     * @param {Array<any>} args - The args array.
+     */
     execute(client, message, args) {
-        return message.channel.send('Pong.');
+        const msg = await message.channel.send('Pinging...');
+
+        const sentTime = msg.editedTimestamp || msg.createdTimestamp;
+        const startTime = message.editedTimestamp || message.createdTimestamp;
+
+        return msg.edit([
+            `Gateway Latency: \`${sentTime - startTime}ms\``,
+            `API Latency: \`${client.ws.ping}ms\``
+        ]);
     }
 }
